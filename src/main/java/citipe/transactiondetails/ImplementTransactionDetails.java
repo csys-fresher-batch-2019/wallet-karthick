4bd94547e8e194af6138f62240d5daa755e18700
Srikarthick S <karrthicks10@gmail.com>
2020-01-29 16:01:36 +0530

    Removed unused imports

--- src/main/java/citipe/transactiondetails/ImplementTransactionDetails.java
+++ src/main/java/citipe/transactiondetails/ImplementTransactionDetails.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
 
 import citipe.Connect;
 import citipe.DAO.TransactionDAO;
-import citipe.bankdatabase.TestDatabase;
-import citipe.bankdatabase.UserDetails;
 
 public class ImplementTransactionDetails implements TransactionDAO {
 
